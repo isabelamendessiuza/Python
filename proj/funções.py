6cd31833a093a83e02653ab82e2313d63190cc48
isabelamendessiuza <131874069+isabelamendessiuza@users.noreply.github.com>
2025-06-16 19:00:32 -0300

    m

---
+++ proj/funções.py
@@ -0,0 +1,21 @@
+
+
+from funcoes import  
+
+introducao()
+mostrar_status()
+
+primeira_escolha()
+mostrar_status()
+
+segunda_escolha()
+mostrar_status()
+
+terceira_escolha()
+mostrar_status()
+
+quarta_escolha()
+mostrar_status()
+
+quinta_escolha()
+mostrar_status()
